09fec5ff41078b61a7c099ac450f88d08642ed1a
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-09 18:05:44 +0100

    Update end.js

--- end.js
+++ end.js
@@ -4,7 +4,6 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-console.log(highScores);
 
 const MAX_HIGH_SCORES = 5;
 
@@ -15,20 +14,16 @@ username.addEventListener('keyup', () => {
 });
 
 saveHighScore = (e) => {
-    console.log('clicked the save button')
     e.preventDefault();
 
     const score = {
-        //score: Math.floor(Math.random() * 100),
         score: mostRecentScore,
         name: username.value,
     };
-    console.log(score);
     highScores.push(score);
-    console.log(highScores);
     highScores.sort((a, b) => b.score - a.score);
     highScores.splice(5);
-    
+
     localStorage.setItem('highScores', JSON.stringify(highScores));
     window.location.assign('/');
 };
