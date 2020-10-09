bcb81f2aad586facf5c6c93fbf6ebd10dd5c6b61
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-09 18:14:47 +0100

    Update highscores.js

--- highscores.js
+++ highscores.js
@@ -1,12 +1,6 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-console.log(highScores);
-
-/*highScores.map(score) => {
-    //console.log('$(score.name)-$(score.score)');
-    console.log('<li class="high-score">$(score.name)-$(score.score)</li>');
-}*/
 highScoresList.innerHTML = highScores
   .map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
